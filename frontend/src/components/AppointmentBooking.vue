e0a2f41628368f6977c97fcf724aead4f3849608
Nenad Skocic <nenad.skocic@gmail.com>
2025-02-06 11:57:10 -0500

    feat: created appt. booking template with vue-cal
    refers to: GOC-18

---
+++ frontend/src/components/AppointmentBooking.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+      <h2>Book a Consultation</h2>
+      <vue-cal :events="appointments" @event-click="selectTimeSlot"></vue-cal>
+    </section>
+  </template>
+  
+  <script>
+  import VueCal from "vue-cal";
+  import "vue-cal/dist/vuecal.css";
+  
+  export default {
+    components: { VueCal },
+    data() {
+      return { appointments: [{ start: "2025-02-05 10:00", end: "2025-02-05 11:00", title: "Available Slot" }] };
+    },
+    methods: {
+      selectTimeSlot(event) {
+        alert(`You booked a meeting at ${event.start}`);
+      },
+    },
+  };
+  </script>
+  
